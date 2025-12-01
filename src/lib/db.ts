3b096c6864b83f73fa73f70bc74939b57a74efc6
Trong Nghia <nghiatrong060505@gmail.com>
2025-12-01 22:53:01 +0700

    Add database configuration and update ProductForm component

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,23 @@
+// import mysql from 'mysql2/promise';
+
+// const pool = mysql.createPool({
+//   host: 'localhost',       // sửa theo config của bạn
+//   user: 'root',            // user mysql của bạn
+//   password: 'nguyenngocanhtuan',      // password mysql của bạn
+//   database: 'quanao', // tên database
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
+// export default pool;
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: 'localhost',       // sửa theo config của bạn
-  user: 'root',            // user mysql của bạn
-  password: 'nguyenngocanhtuan',      // password mysql của bạn
-  database: 'quanao', // tên database
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
